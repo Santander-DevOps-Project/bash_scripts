cb9e1be38fc556370f911b6fc30857d6e2931ac0
wasty30 <elvisopokumensah26@yahoo.com>
2024-07-25 11:29:47 +0000

    make bash script dynamic

--- flow_controls.sh
+++ flow_controls.sh
@@ -1,2 +1,1 @@
-#!/bin/bash																			echo $USER
-if [[ $USER = ec2-user" ]] ; then	 															echo "" yes,we are running an amazon linux"															fi 
+#!/bin/bash																			echo $USER																			if [[ $USER = ec2-user ]] ; then																echo "" yes,we are running an amazon linux"														fi		
